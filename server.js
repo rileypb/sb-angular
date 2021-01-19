8c1bb8b7a53bf9abec3e629e09489cdca0df25e1
Phil Riley <rileypb@gmail.com>
2021-01-19 14:13:09 -0500

    playing with server.js

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('./dist/scrumboard-ui'));
+app.use(express.static('./dist'));
 
 app.get('/*', function (req, res) {
-  res.sendFile('index.html', { root: 'scrumboard-ui' }
+  res.sendFile('index.html', { root: '' }
   );
 });
 
