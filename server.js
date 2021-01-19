716550b6c9b9152f6eb44f414361cd81d04434fa
Phil Riley <rileypb@gmail.com>
2021-01-19 15:36:13 -0500

    Update server.js

--- server.js
+++ server.js
@@ -7,14 +7,14 @@ const app = express();
 app.use(express.static('./dist'));
 
 app.get('/*', function (req, res) {
-  res.sendFile('index.html', { root: '' }
+  res.sendFile('index.html', { root: '/' }
   );
 });
 
-app.use('/api', proxy('http://scrumboard-api.herokuapp.com'));
-app.use('/admin', proxy('http://scrumboard-api.herokuapp.com'));
-app.use('/users', proxy('http://scrumboard-api.herokuapp.com'));
-app.use('/assets', proxy('http://scrumboard-api.herokuapp.com'));
+app.use('/api', proxy('scrumboard-api.herokuapp.com'));
+app.use('/admin', proxy('scrumboard-api.herokuapp.com'));
+app.use('/users', proxy('scrumboard-api.herokuapp.com'));
+app.use('/assets', proxy('scrumboard-api.herokuapp.com'));
 
 app.listen(process.env.PORT || 4200);
 
