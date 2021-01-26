f1d0f36279b100c29cc6abd23a34e66f1c78fe0f
Phil Riley <rileypb@gmail.com>
2021-01-26 10:47:31 -0500

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static('./dist'));
 
 app.get('/*', function (req, res) {
-  res.sendFile('index.html', { root: '/' }
+  res.sendFile('', { root: '/' }
   );
 });
 
