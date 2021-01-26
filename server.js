be860ef596c962e6fca54bb8936237d146df3dfe
Phil Riley <rileypb@gmail.com>
2021-01-26 10:37:08 -0500

    make express serve * as index.html

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const app = express();
 
 app.use(express.static('./dist'));
 
+app.get('/*', function (req, res) {
+  res.sendFile('index.html', { root: '/' }
+  );
+});
+
 app.listen(process.env.PORT || 4200);
 
 console.log(`Running on port ${process.env.PORT || 4200}`)
