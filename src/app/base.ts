4f1805d7425e581404793414104efcbb07d8f466
Phil Riley <rileypb@gmail.com>
2021-03-13 14:22:29 -0500

    tweak base.rb

--- src/app/base.ts
+++ src/app/base.ts
@@ -1,5 +1,5 @@
 export class Base {
   permission(model:any, type:string):boolean {
-	return model && model.permissions && model.permissions.indexOf(type) != -1
+	return model?.permissions?.indexOf(type) != -1
   }
 }
