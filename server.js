9905ac376ec799a87557e88ff38a566a8ab615ef
Phil Riley <rileypb@gmail.com>
2021-04-09 21:52:46 -0400

    redirect http to https

--- server.js
+++ server.js
@@ -5,6 +5,15 @@ const app = express();
 
 app.use(express.static('./dist'));
 
+app.use((req, res, next) => {
+    // if (req.headers.host === 'your-app.herokuapp.com')
+    //     return res.redirect(301, 'https://www.your-custom-domain.com');
+    if (req.headers['x-forwarded-proto'] !== 'https')
+        return res.redirect('https://' + req.headers.host + req.url);
+    else
+        return next();
+});
+
 app.get('/*', function (req, res) {
   res.sendFile('index.html', { root: './dist' }
   );
