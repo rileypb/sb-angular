53f744bb3885923ceb19deec316355687d205a91
Phil Riley <rileypb@gmail.com>
2021-12-16 23:30:07 -0500

    add sprint compare to retrospective page

--- src/app/sprint-retrospective-report-inner/sprint-retrospective-report-inner.component.ts
+++ src/app/sprint-retrospective-report-inner/sprint-retrospective-report-inner.component.ts
@@ -2,19 +2,24 @@ import { Component, OnInit, Input } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { SprintsService } from '../sprints.service';
 import { callWithSnackBar } from '../util';
+import { Base } from '../base';
 
 @Component({
   selector: 'sb-sprint-retrospective-report-inner',
   templateUrl: './sprint-retrospective-report-inner.component.html',
   styleUrls: ['./sprint-retrospective-report-inner.component.css']
 })
-export class SprintRetrospectiveReportInnerComponent implements OnInit {
+export class SprintRetrospectiveReportInnerComponent extends Base implements OnInit {
   @Input() report:any;
   @Input() sprintId:number;
 
+  mode:string = "report";
+
   editingRetrospective:boolean;
 
-  constructor(private sprintsService:SprintsService, private snackBar:MatSnackBar) { }
+  constructor(private sprintsService:SprintsService, private snackBar:MatSnackBar) {
+    super();
+  }
 
   ngOnInit(): void {
   }
